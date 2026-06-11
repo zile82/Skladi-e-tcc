f5c1c4692bcacb65ab63b4bb466bfb97900afa79
zile82 <Adi_iba@yahoo.com>
2026-06-11 22:54:48 +0200

    Update ai_studio_code.py

--- ai_studio_code.py
+++ ai_studio_code.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
+from PIL import Image, ImageOps
 
 st.set_page_config(page_title="Skladiste Cloud", layout="wide")
 
