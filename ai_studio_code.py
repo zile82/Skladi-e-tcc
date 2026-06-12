387b821250eff0e99b0007c3e1518aa0ad82784b
zile82 <Adi_iba@yahoo.com>
2026-06-12 16:40:08 +0200

    Update ai_studio_code.py

--- ai_studio_code.py
+++ ai_studio_code.py
@@ -22,8 +22,7 @@ naziv = st.sidebar.text_input("Naziv")
 lokacija = st.sidebar.selectbox("Lokacija", ["Hala 1", "Hala 2", "Regal A", "Regal B"])
 kolicina = st.sidebar.number_input("Količina", min_value=0, step=1)
 datum_unosa = st.date_input("Datum ulaza", value=None)
-submitted = 
-st.form_submit_button("Ažuriraj stanje")
+submitted = st.form_submit_button("Ažuriraj stanje")
 if st.sidebar.button("Ažuriraj stanje"):
     # Provjeri postoji li ean
     if ean in df['ean'].values:
