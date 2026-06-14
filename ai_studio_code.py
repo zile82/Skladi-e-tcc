e43999dcea892bccbc392b2514e8c3f30689fc3a
zile82 <Adi_iba@yahoo.com>
2026-06-14 18:31:04 +0200

    Update ai_studio_code.py

--- ai_studio_code.py
+++ ai_studio_code.py
@@ -31,7 +31,7 @@ if ean :
     datum_str = datum_vhoda.strftime("%d, %m, %Y") if datum_vhoda else ""
 # 1. provjeri postoji li artikl (koristimo ean kolonu)
 # osiguramo da su svi ean-ovi u tablici stringovi radi usporedbe
-    ean_lista = df_fresh['ean'].astype(str).str.replace(r'\.0$', '',regex=True).values
+    ean_lista = df_fresh['ean'].astype(str).str.replace(r'\.0$', '', regex=True).values
      if str(ean) in ean_lista:
                 # azuriranje - dodamo datum_str na kraju liste
         df_fresh.loc[df_fresh['ean'].astype(str).str.replace(r'\.0$', '',
